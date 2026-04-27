6cca0066fa4ba77be05009a226a272b8b855e82b
DevJuan001 <juanesyt7@gmail.com>
2026-04-27 18:41:29 -0500

    feat: rename guarantee methods to warranties for consistency

--- app/controllers/warranties_controller.py
+++ app/controllers/warranties_controller.py
@@ -6,12 +6,12 @@ from app.models.warranties_model import Warranty, WarrantyUpdate
 class WarrantiesController:
 
     @staticmethod
-    def get_all_guarantee(
+    def get_all_warranties(
         start_date: str = None,
         end_date: str = None,
         status: int = None,
     ):
-        error, guarantiee = WarrantiesRepository.find_all_guarantiee(
+        error, warranties = WarrantiesRepository.find_all_warranties(
             start_date,
             end_date,
             status,
@@ -19,21 +19,21 @@ class WarrantiesController:
         if error:
             raise HTTPException(status_code=404, detail=error)
         return {
-            "data": guarantiee
+            "data": warranties
         }
 
     @staticmethod
-    def get_guarantiee_by_id(warranty_incidents_id: int):
-        error, guarantiee = WarrantiesRepository.find_by_id(
+    def get_warranty_by_id(warranty_incidents_id: int):
+        error, warranties = WarrantiesRepository.find_by_id(
             warranty_incidents_id)
         if error:
             raise HTTPException(status_code=404, detail=error)
         return {
-            "data": guarantiee
+            "data": warranties
         }
 
     @staticmethod
-    def create_guarantiee(warranty_data: Warranty):
+    def create_warranty(warranty_data: Warranty):
         error, success, message = WarrantiesRepository.create(warranty_data)
         if error:
             raise HTTPException(status_code=400, detail=error)
@@ -43,7 +43,7 @@ class WarrantiesController:
         }
 
     @staticmethod
-    def update_garantee(warranty_incidents_id: int, warranty_data: WarrantyUpdate):
+    def update_warranty(warranty_incidents_id: int, warranty_data: WarrantyUpdate):
         error, success, message = WarrantiesRepository.update(
             warranty_incidents_id, warranty_data)
         if error:
@@ -54,7 +54,7 @@ class WarrantiesController:
         }
 
     @staticmethod
-    def delete_garantee(warranty_incidents_id: int):
+    def delete_warranty(warranty_incidents_id: int):
         error, success, message = WarrantiesRepository.delete(
             warranty_incidents_id)
         if error:
@@ -65,31 +65,22 @@ class WarrantiesController:
         }
 
     @staticmethod
-    def get_deleted_guarantees_by_date_range(start_date: str, end_date: str):
-        error, guarantees = WarrantiesRepository.find_deleted_guarantees_by_date_range(
+    def get_deleted_warranties_by_date_range(start_date: str, end_date: str):
+        error, warranties = WarrantiesRepository.find_deleted_warranties_by_date_range(
             start_date, end_date)
 
         if error:
             raise HTTPException(status_code=404, detail=error)
         return {
-            "data": guarantees
+            "data": warranties
         }
 
     @staticmethod
-    def get_all_guarantees():
-        error, guarantees = WarrantiesRepository.find_all_guarantees()
-        if error:
-            raise HTTPException(status_code=404, detail=error)
-        return {
-            "data": guarantees
-        }
-
-    @staticmethod
-    def get_disabled_guarantees():
-        error, guarantees = WarrantiesRepository.find_disabled_guarantees()
+    def get_disabled_warranties():
+        error, warranties = WarrantiesRepository.find_disabled_warranties()
 
         if error:
             raise HTTPException(status_code=404, detail=error)
         return {
-            "data": guarantees
+            "data": warranties
         }
