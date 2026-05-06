43ed2bf30c1cb582f11edf6d635179f2adb9c174
DevJuan001 <juanesyt7@gmail.com>
2026-05-05 23:43:48 -0500

    feat: add CreateOutputDetails model for output order details management

--- app/features/output_orders/models/output_details_model.py
+++ app/features/output_orders/models/output_details_model.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
-class OutputDetails(BaseModel):
+class CreateOutputDetails(BaseModel):
     product_serial: str 
     output_details_id: Optional[int] = None
     out_product_garanty: datetime
