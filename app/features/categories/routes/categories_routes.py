916a9e11854065f4571173d2a46c0ecf89903e35
DevJuan001 <juanesyt7@gmail.com>
2026-05-10 18:45:57 -0500

    refactor: simplify role dependencies in category update and disable/enable endpoints

--- app/features/categories/routes/categories_routes.py
+++ app/features/categories/routes/categories_routes.py
@@ -10,8 +10,6 @@ router = APIRouter(
 )
 
 # Endpoint para obtener todas las categorias
-
-
 @router.get(
     "/",
     dependencies=[
@@ -19,12 +17,10 @@ router = APIRouter(
         Depends(require_roles(["Admin", "Almacén", "Técnico"]))
     ]
 )
-def get_all_categories(filters: CategoriesFilters):
+def get_all_categories(filters: CategoriesFilters = Depends()):
     return CategoriesController.get_all_categories(filters)
 
 # Endpoint para obtener una categoria mediante el id
-
-
 @router.get(
     "/{category_id}",
     dependencies=[
@@ -35,7 +31,7 @@ def get_all_categories(filters: CategoriesFilters):
 def get_category_by_id(category_id: int):
     return CategoriesController.get_category_by_id(category_id)
 
-
+# Endpoint para crear una categoría
 @router.post(
     "/create",
     dependencies=[
@@ -46,34 +42,34 @@ def get_category_by_id(category_id: int):
 def create_category(category_data: CreateCategory):
     return CategoriesController.create_category(category_data)
 
-
+# Endpoint para actualizar una categoría
 @router.put(
     "/update/{category_id}",
     dependencies=[
         Depends(RateLimiter(times=30, seconds=60)),
-        Depends(require_roles(["Admin", "Almacén", "Técnico"]))
+        Depends(require_roles(["Admin", "Almacén"]))
     ]
 )
 def update_category(category_id: int, category_data: UpdateCategory):
     return CategoriesController.update_category(category_id, category_data)
 
-
+# Endpoint para deshabilitar una categoría
 @router.put(
     "/disable/{category_id}",
     dependencies=[
         Depends(RateLimiter(times=30, seconds=60)),
-        Depends(require_roles(["Admin", "Almacén", "Técnico"]))
+        Depends(require_roles(["Admin", "Almacén"]))
     ]
 )
 def disable_category(category_id: int):
     return CategoriesController.disable_category(category_id)
 
-
+# Endpoint para habilitar una categoría
 @router.put(
     "/enable/{category_id}",
     dependencies=[
         Depends(RateLimiter(times=30, seconds=60)),
-        Depends(require_roles(["Admin", "Almacén", "Técnico"]))
+        Depends(require_roles(["Admin", "Almacén"]))
     ]
 )
 def enable_category(category_id: int):
