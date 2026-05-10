b534baa2b7397a467fd90def055446d89320e628
DevJuan001 <juanesyt7@gmail.com>
2026-05-10 18:46:01 -0500

    refactor: integrate CategoriesService into CategoriesController for improved category management

--- app/features/categories/controllers/categories_controller.py
+++ app/features/categories/controllers/categories_controller.py
@@ -1,59 +1,54 @@
-from app.repository.category_repository import CategoryRepository
-from app.models.category_model import CategoryCreate, CategoryUpdate
 from fastapi import HTTPException
+from app.features.categories.services.categories_service import CategoriesService
+from app.features.categories.models.categories_model import CategoriesFilters, CreateCategory, UpdateCategory
 
 
 class CategoriesController:
 
     @staticmethod
-    def get_all_categories(
-        name_order: str = None,
-        start_date: str = None,
-        end_date: str = None,
-        status: int = None,
-    ):
-        error, categories = CategoryRepository.find_all_categories(
-            name_order,
-            start_date,
-            end_date,
-            status,
-        )
+    def get_all_categories(filters: CategoriesFilters):
+        error, categories = CategoriesService.get_all_categories(filters)
 
         if error:
             raise HTTPException(status_code=400, detail=error)
 
         return {
-            "message": "Categorías obtenidas correctamente",
             "data": categories
         }
 
     @staticmethod
     def get_category_by_id(category_id: int):
-        error, category = CategoryRepository.find_by_id(category_id)
+        error, category = CategoriesService.get_category_by_id(category_id)
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": category
         }
 
     @staticmethod
-    def create_category(category_data: CategoryCreate):
-        data_dict = category_data.dict()
+    def create_category(category_data: CreateCategory):
+        error, success, message = CategoriesService.create_category(
+            category_data)
 
-        error, success, message = CategoryRepository.create_category(data_dict)
         if error:
             raise HTTPException(status_code=400, detail=error)
+
         return {
             "success": success,
             "message": message,
         }
 
     @staticmethod
-    def update_category(category_id: int, category_data: CategoryUpdate):
-        error, success, message = CategoryRepository.update_category(
-            category_id, category_data)
+    def update_category(category_id: int, category_data: UpdateCategory):
+        error, success, message = CategoriesService.update_category(
+            category_id, category_data
+        )
+
         if error:
             raise HTTPException(status_code=400, detail=error)
+
         return {
             "success": success,
             "message": message
@@ -61,10 +56,13 @@ class CategoriesController:
 
     @staticmethod
     def enable_category(category_id: int):
-        error, success, message = CategoryRepository.enable_category(
-            category_id)
+        error, success, message = CategoriesService.enable_category(
+            category_id
+        )
+
         if error:
             raise HTTPException(status_code=400, detail=error)
+
         return {
             "success": success,
             "message": message
@@ -72,10 +70,13 @@ class CategoriesController:
 
     @staticmethod
     def disable_category(category_id: int):
-        error, success, message = CategoryRepository.disable_category(
-            category_id)
+        error, success, message = CategoriesService.disable_category(
+            category_id
+        )
+
         if error:
             raise HTTPException(status_code=400, detail=error)
+
         return {
             "success": success,
             "message": message
