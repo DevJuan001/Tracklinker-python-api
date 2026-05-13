e398fdf5b85cd72d143de8d93dc2edbd9342c3cd
DevJuan001 <juanesyt7@gmail.com>
2026-05-13 16:29:59 -0500

    feat: add schemas for output order filters and creation/updating

---
+++ app/features/output_orders/models/output_orders_schema.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class OutputOrdersFiltersSchema(BaseModel):
+    start_date: Optional[str] = None
+    end_date: Optional[str] = None
+    status: Optional[int] = None
+
+
+class CreateOutputOrderSchema(BaseModel):
+    product_serial: str
+    output_product_garanty: datetime
+
+
+class UpdateOutputOrderSchema(BaseModel):
+    output_order_status: Optional[int] = None
+    product_serial: Optional[str] = None
+    output_product_garanty: Optional[datetime] = None
