9dd13c09daddbc1e0c925895448e5869c14e1ad1
DevJuan001 <juanesyt7@gmail.com>
2026-05-14 18:54:22 -0500

    refactor: update ProductsService to utilize new schema and entity models, enhancing error handling

--- app/features/products/services/products_service.py
+++ app/features/products/services/products_service.py
@@ -1,20 +1,22 @@
+
 from app.utils.logger import get_logger
 from app.core.database import get_connection
 from app.core.exception import ServiceError
 from app.features.products.repositories.products_repository import ProductsRepository
 from app.features.warranties.repositories.warranties_repository import WarrantiesRepository
-from app.features.products.repositories.product_details_repository import ProductDetailsRepository
 from app.features.products.repositories.product_serials_repository import ProductSerialsRepository
-from app.features.products.models.product_model import ProductsFilter, UpdateProduct, CreateProduct, UpdateProductStatus
-from app.features.products.models.product_serial_model import CreateProductSerial, UpdateProductSerial
-from app.features.products.models.product_details_model import CreateProductDetails, UpdateProductDetails
+from app.features.products.repositories.product_details_repository import ProductDetailsRepository
+from app.features.products.models.schemas.product_serials_schemas import CreateProductSerialSchema, UpdateProductSerialSchema
+from app.features.products.models.entities.product_details_entity import CreateProductDetailsEntity, UpdateProductDetailsEntity
+from app.features.products.models.schemas.products_schemas import CreateProductSchema, ProductsFilterSchema, UpdateProductSchema, UpdateProductStatusSchema
+
 
 logger = get_logger("products.service")
 
 
 class ProductsService:
     @staticmethod
-    def get_all_products(filters: ProductsFilter):
+    def get_all_products(filters: ProductsFilterSchema):
         connection = get_connection()
 
         try:
@@ -22,12 +24,17 @@ class ProductsService:
                 filters, connection
             )
             if error:
-                return "Error al intentar obtener los productos", None
+                raise ServiceError(error)
 
             return None, products
+
+        except ServiceError as e:
+            return e.message, None
+
         except Exception as e:
             logger.error("Error en get_all_products: %s", e, exc_info=True)
             return "Error al intentar obtener los productos", None
+
         finally:
             connection.close()
 
@@ -39,10 +46,15 @@ class ProductsService:
             error, products = ProductsRepository.find_all_product_status(
                 connection
             )
+
             if error:
-                return "Error al intentar obtener los estados", None
+                raise ServiceError(error)
 
             return None, products
+
+        except ServiceError as e:
+            return e.message, None
+
         except Exception as e:
             logger.error(
                 "Error en get_all_products_status: %s",
@@ -50,18 +62,19 @@ class ProductsService:
                 exc_info=True
             )
             return "Error al intentar obtener los estados", None
+
         finally:
             connection.close()
 
     @staticmethod
-    def create_product(product_data: CreateProduct):
+    def create_product(product_data: CreateProductSchema):
         data = product_data.model_dump()
 
         connection = get_connection()
 
         try:
             error, success, message, product_details_id = ProductDetailsRepository.create_product_details(
-                CreateProductDetails(
+                CreateProductDetailsEntity(
                     model_id=data["model_id"],
                 ), connection)
 
@@ -72,13 +85,18 @@ class ProductsService:
                 data, product_details_id, connection
             )
 
+            if error is not None or not success:
+                raise ServiceError(error)
+
             error, success, message = ProductSerialsRepository.create_product_serial(
-                CreateProductSerial(
+                CreateProductSerialSchema(
                     product_serial=data["product_serial"],
                     product_id=product_id,
                     input_order_id=data["input_order_id"],
                     warranty_time=data["warranty_time"]
-                ), connection)
+                ),
+                connection
+            )
 
             if error is not None or not success:
                 raise ServiceError(error)
@@ -94,11 +112,12 @@ class ProductsService:
             connection.rollback()
             logger.error("Error en create_product: %s", e, exc_info=True)
             return "Error al intentar crear el producto", False, None
+
         finally:
             connection.close()
 
     @staticmethod
-    def update_product(product_data: UpdateProduct):
+    def update_product(product_data: UpdateProductSchema):
         data = product_data.model_dump(exclude_none=True)
 
         connection = get_connection()
@@ -110,16 +129,16 @@ class ProductsService:
             )
 
             if not product:
-                return "Producto no encontrado", False, None
+                raise ServiceError("Producto no encontrado")
 
             # Actualizar details si vino brand o model
             if details_fields := {
                 key: data[key]
-                for key in ["brand_id", "model_id"]
+                for key in ["model_id"]
                 if key in data
             }:
                 error, success, message = ProductDetailsRepository.update_product_details(
-                    UpdateProductDetails(
+                    UpdateProductDetailsEntity(
                         product_details_id=data["product_details_id"], **details_fields
                     ),
                     connection
@@ -134,7 +153,7 @@ class ProductsService:
                 if key in data
             }:
                 error, success, message = ProductSerialsRepository.update_product_serial(
-                    UpdateProductSerial(id=data["id"], **serial_fields),
+                    UpdateProductSerialSchema(id=data["id"], **serial_fields),
                     connection
                 )
                 if error or not success:
@@ -144,8 +163,12 @@ class ProductsService:
                 data, connection
             )
 
+            if error or not success:
+                raise ServiceError(error)
+
             connection.commit()
-            return error, success, message
+
+            return None, True, "Producto actualizado correctamente"
 
         except ServiceError as e:
             return e.message, False, None
@@ -153,11 +176,12 @@ class ProductsService:
         except Exception as e:
             logger.error("Error en update_product: %s", e, exc_info=True)
             return "Error al intentar actualizar el producto", False, None
+
         finally:
             connection.close()
 
     @staticmethod
-    def update_product_status(product_data: UpdateProductStatus):
+    def update_product_status(product_data: UpdateProductStatusSchema):
         data = product_data.model_dump()
 
         connection = get_connection()
@@ -204,5 +228,6 @@ class ProductsService:
                 exc_info=True
             )
             return "Error al intentar actualizar el estado producto", False, None
+
         finally:
             connection.close()
