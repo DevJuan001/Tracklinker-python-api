6c8100ee00fa7037227f08a016c917885642c6a1
DevJuan001 <juanesyt7@gmail.com>
2026-05-14 18:06:32 -0500

    refactor: enhance error handling in InputOrdersService and update input order data model usage

--- app/features/products/services/input_orders_service.py
+++ app/features/products/services/input_orders_service.py
@@ -1,7 +1,11 @@
-from app.utils.logger import get_logger
+
+
 from app.core.database import get_connection
-from app.features.products.models.input_order_model import CreateInputOrder
+from app.utils.logger import get_logger
+from app.core.exception import ServiceError
+from app.features.products.models.input_orders_schemas import CreateInputOrderSchema
 from app.features.products.repositories.input_orders_repository import InputOrdersRepository
+
 
 logger = get_logger("input_orders.service")
 
@@ -15,17 +19,21 @@ class InputOrdersService:
             error, products = InputOrdersRepository.find_all_input_orders(
                 connection
             )
+
             if error:
-                return "Error al intentar obtener las ordenes de entrada", None
+                raise ServiceError(error)
 
             return None, products
+
+        except ServiceError as e:
+            return e.message, None
+
         except Exception as e:
-            connection.rollback()
             logger.error("Error en get_all_input_orders: %s", e, exc_info=True)
             return "Error al intentar obtener las ordenes de entrada", None
 
     @staticmethod
-    def create_input_order(input_order_data: CreateInputOrder):
+    def create_input_order(input_order_data: CreateInputOrderSchema):
         data = input_order_data.model_dump()
 
         connection = get_connection()
@@ -34,12 +42,17 @@ class InputOrdersService:
             error, success, message = InputOrdersRepository.create_input_order(
                 data, connection
             )
-            if error:
-                return "Error al intentar crear la orden de entrada", False, None
+
+            if error or not success:
+                raise ServiceError(error)
 
             connection.commit()
 
             return None, True, "Orden de entrada creada correctamente"
+
+        except ServiceError as e:
+            return e.message, False, None
+
         except Exception as e:
             connection.rollback()
             logger.error("Error en create_input_order: %s", e, exc_info=True)
