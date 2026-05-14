f75aa6dee58b8e590076f7af165070f8e230c6d2
DevJuan001 <juanesyt7@gmail.com>
2026-05-14 18:57:40 -0500

    refactor: migrate ReportsController methods to utilize ReportsService for improved modularity and maintainability

--- app/features/reports/controllers/reports_controller.py
+++ app/features/reports/controllers/reports_controller.py
@@ -1,11 +1,7 @@
+
+
 from fastapi import HTTPException
-from app.features.users.repositories.users_repository import UsersRepository
-from app.features.products.repositories.products_repository import ProductsRepository
-from app.features.suppliers.repositories.suppliers_repository import SuppliersRepository
-from app.features.categories.repositories.categories_repository import CategoriesRepository
-from app.features.warranties.repositories.warranties_repository import WarrantiesRepository
-from app.features.output_orders.repositories.output_orders_repository import OutputOrdersRepository
-from app.features.subcategories.repositories.subcategories_repository import SubcategoriesRepository
+from app.features.reports.services.reports_service import ReportsService
 
 
 class ReportsController:
@@ -13,45 +9,44 @@ class ReportsController:
     #   ------------ REPORTES DE USUARIOS ------------
     @staticmethod
     def get_recent_users():
-        error, user = UsersRepository.find_recent_users()
+        error, users = ReportsService.get_recent_users()
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
-            "data": user
+            "data": users
         }
 
     @staticmethod
     def get_users_by_rol(period: str):
-        error, user = UsersRepository.find_users_by_rol(period)
+        error, users = ReportsService.get_users_by_rol(period)
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
-            "data": user
+            "data": users
         }
 
     @staticmethod
-    def get_users_by_month(period: str):
-        error, data = UsersRepository.find_users_by_month(period)
-        if error:
-            raise HTTPException(status_code=404, detail=error)
-        return {
-            "data": data
-        }
+    def get_users_growth(period: str):
+        error, data = ReportsService.get_users_growth(period)
 
-    @staticmethod
-    def get_user_growth(period: str):
-        error, data = UsersRepository.find_users_growth(period)
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
 
     @staticmethod
     def get_users_by_status():
-        error, data = UsersRepository.find_users_by_status()
+        error, data = ReportsService.get_users_by_status()
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
@@ -59,36 +54,44 @@ class ReportsController:
 #   ------------ REPORTES DE PRODUCTOS ------------
     @staticmethod
     def get_recent_products():
-        error, products = ProductsRepository.find_recent_products()
+        error, products = ReportsService.get_recent_products()
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": products
         }
 
     @staticmethod
     def get_products_growth(period: str):
-        error, data = ProductsRepository.find_products_growth(period)
+        error, data = ReportsService.get_products_growth(period)
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
 
     @staticmethod
     def get_products_by_brand(period: str):
-        error, data = ProductsRepository.find_products_by_brand(period)
+        error, data = ReportsService.get_products_by_brand(period)
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
 
     @staticmethod
     def get_products_by_status():
-        error, data = ProductsRepository.find_products_by_status()
+        error, data = ReportsService.get_products_by_status()
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
@@ -98,36 +101,44 @@ class ReportsController:
 
     @staticmethod
     def get_recent_categories():
-        error, categories = CategoriesRepository.find_recent_categories()
+        error, categories = ReportsService.get_recent_categories()
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": categories
         }
 
     @staticmethod
     def get_categories_growth(period: str):
-        error, data = CategoriesRepository.find_categories_growth(period)
+        error, data = ReportsService.get_categories_growth(period)
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
 
     @staticmethod
     def get_categories_by_brand(period: str):
-        error, data = CategoriesRepository.find_categories_by_brand(period)
+        error, data = ReportsService.get_categories_by_brand(period)
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
 
     @staticmethod
     def get_categories_by_status():
-        error, data = CategoriesRepository.find_categories_by_status()
+        error, data = ReportsService.get_categories_by_status()
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
@@ -135,37 +146,46 @@ class ReportsController:
 #   ------------ REPORTES DE SUBCATEGORIAS ------------
     @staticmethod
     def get_recent_subcategories():
-        error, subcategories = SubcategoriesRepository.find_recent_subcategories()
+        error, subcategories = ReportsService.get_recent_subcategories()
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": subcategories
         }
 
     @staticmethod
     def get_subcategories_growth(period: str):
-        error, data = SubcategoriesRepository.find_subcategories_growth(period)
+        error, data = ReportsService.get_subcategories_growth(period)
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
 
     @staticmethod
     def get_subcategories_by_category(period: str):
-        error, data = SubcategoriesRepository.find_subcategories_by_category(
-            period)
+        error, data = ReportsService.get_subcategories_by_category(
+            period
+        )
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
 
     @staticmethod
     def get_subcategories_by_status():
-        error, data = SubcategoriesRepository.find_subcategories_by_status()
+        error, data = ReportsService.get_subcategories_by_status()
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
@@ -173,36 +193,44 @@ class ReportsController:
 #   ------------ REPORTES DE GARANTÍAS ------------
     @staticmethod
     def get_recent_warranties():
-        error, warranties = WarrantiesRepository.find_recent_warranties()
+        error, warranties = ReportsService.get_recent_warranties()
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": warranties
         }
 
     @staticmethod
     def get_warranties_growth(period: str):
-        error, data = WarrantiesRepository.find_warranties_growth(period)
+        error, data = ReportsService.get_warranties_growth(period)
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
 
     @staticmethod
     def get_warranties_by_brand(period: str):
-        error, data = WarrantiesRepository.find_warranties_by_brand(period)
+        error, data = ReportsService.get_warranties_by_brand(period)
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
 
     @staticmethod
     def get_warranties_by_status():
-        error, data = WarrantiesRepository.find_warranties_by_status()
+        error, data = ReportsService.get_warranties_by_status()
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
@@ -210,36 +238,44 @@ class ReportsController:
 #   ------------ REPORTES DE PROVEEDORES ------------
     @staticmethod
     def get_recent_suppliers():
-        error, suppliers = SuppliersRepository.find_recent_suppliers()
+        error, suppliers = ReportsService.get_recent_suppliers()
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": suppliers
         }
 
     @staticmethod
     def get_suppliers_growth(period: str):
-        error, data = SuppliersRepository.find_suppliers_growth(period)
+        error, data = ReportsService.get_suppliers_growth(period)
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
 
     @staticmethod
     def get_suppliers_by_brand(period: str):
-        error, data = SuppliersRepository.find_suppliers_by_brand(period)
+        error, data = ReportsService.get_suppliers_by_brand(period)
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
 
     @staticmethod
     def get_suppliers_by_status():
-        error, data = SuppliersRepository.find_suppliers_by_status()
+        error, data = ReportsService.get_suppliers_by_status()
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
@@ -249,36 +285,44 @@ class ReportsController:
 
     @staticmethod
     def get_recent_outputs():
-        error, outputs = OutputOrdersRepository.find_recent_outputs()
+        error, outputs = ReportsService.get_recent_outputs()
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": outputs
         }
 
     @staticmethod
     def get_outputs_growth(period: str):
-        error, data = OutputOrdersRepository.find_outputs_growth(period)
+        error, data = ReportsService.get_outputs_growth(period)
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
 
     @staticmethod
     def get_outputs_by_brand(period: str):
-        error, data = OutputOrdersRepository.find_outputs_by_brand(period)
+        error, data = ReportsService.get_outputs_by_brand(period)
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
 
     @staticmethod
     def get_outputs_by_status():
-        error, data = OutputOrdersRepository.find_outputs_by_status()
+        error, data = ReportsService.get_outputs_by_status()
+
         if error:
             raise HTTPException(status_code=404, detail=error)
+
         return {
             "data": data
         }
