fdf785220ec26bf9b1bad7b2f48bf7e8740756e8
DevJuan001 <juanesyt7@gmail.com>
2026-05-17 15:26:11 -0500

    feat: add new response models for output orders to enhance data handling and reporting

--- app/features/output_orders/models/output_orders_responses.py
+++ app/features/output_orders/models/output_orders_responses.py
@@ -1,6 +1,8 @@
 from datetime import date
 from pydantic import BaseModel
 
+from app.utils.base_schema import BaseSchema
+
 
 class OutputOrderResponse(BaseModel):
     output_order_id: int
@@ -12,3 +14,30 @@ class OutputOrderResponse(BaseModel):
     product_brand_name: str
     product_model_name: str
     product_model_description: str
+
+
+class RecentOutputOrderResponse(BaseSchema):
+    id: int
+    serial: str
+    brand: str
+    model: str
+    warranty_time: str
+    date: str
+    status: int
+
+
+class OutputOrderByBrandResponse(BaseModel):
+    brand: str
+    outputs: int
+
+
+class OutputOrderByStatusResponse(BaseModel):
+    total_outputs: int
+    recent_outputs: int
+    inactive_outputs: int
+    active_outputs: int
+
+
+class OutputOrderGrowthResponse(BaseSchema):
+    date: str
+    output_orders: int
