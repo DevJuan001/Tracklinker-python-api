9e75d36758c591278ff7d43f0ae8b7eeda14e00d
DevJuan001 <juanesyt7@gmail.com>
2026-05-24 16:49:48 -0500

    fix: update role dependencies in subcategory routes to include 'Almacén' and 'Técnico'

--- app/features/subcategories/routes/subcategories_routes.py
+++ app/features/subcategories/routes/subcategories_routes.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends
 from fastapi_limiter.depends import RateLimiter
 
+from app.middlewares.roles_middleware import require_roles
 from app.features.subcategories.controllers.subcategories_controller import SubcategoriesController
 from app.features.subcategories.models.subcategories_schemas import CreateSubcategorySchema, SubcategoriesFiltersSchema, UpdateSubcategorySchema
-from app.middlewares.roles_middleware import require_roles
 
 
 router = APIRouter(
@@ -17,7 +17,7 @@ router = APIRouter(
     "/",
     dependencies=[
         Depends(RateLimiter(times=30, seconds=60)),
-        Depends(require_roles(["Admin"]))
+        Depends(require_roles(["Admin", "Almacén", "Técnico"]))
     ]
 )
 def get_all_subcategories(filters: SubcategoriesFiltersSchema = Depends()):
@@ -29,7 +29,7 @@ def get_all_subcategories(filters: SubcategoriesFiltersSchema = Depends()):
     "/active-categories",
     dependencies=[
         Depends(RateLimiter(times=30, seconds=60)),
-        Depends(require_roles(["Admin"]))
+        Depends(require_roles(["Admin", "Almacén"]))
     ]
 )
 def get_active_categories():
@@ -41,7 +41,7 @@ def get_active_categories():
     "/{subcategory_id}",
     dependencies=[
         Depends(RateLimiter(times=30, seconds=60)),
-        Depends(require_roles(["Admin"]))
+        Depends(require_roles(["Admin", "Almacén"]))
     ]
 )
 def get_subcategory_by_id(subcategory_id: int):
@@ -53,7 +53,7 @@ def get_subcategory_by_id(subcategory_id: int):
     "/create",
     dependencies=[
         Depends(RateLimiter(times=30, seconds=60)),
-        Depends(require_roles(["Admin"]))
+        Depends(require_roles(["Admin", "Almacén"]))
     ]
 )
 def create_subcategory(
@@ -67,7 +67,7 @@ def create_subcategory(
     "/update/{subcategory_id}",
     dependencies=[
         Depends(RateLimiter(times=30, seconds=60)),
-        Depends(require_roles(["Admin"]))
+        Depends(require_roles(["Admin", "Almacén"]))
     ]
 )
 def update_subcategory(
@@ -82,7 +82,7 @@ def update_subcategory(
     "/disable/{subcategory_id}",
     dependencies=[
         Depends(RateLimiter(times=30, seconds=60)),
-        Depends(require_roles(["Admin"]))
+        Depends(require_roles(["Admin", "Almacén"]))
     ]
 )
 def disable_subcategory(
@@ -96,7 +96,7 @@ def disable_subcategory(
     "/enable/{subcategory_id}",
     dependencies=[
         Depends(RateLimiter(times=30, seconds=60)),
-        Depends(require_roles(["Admin"]))
+        Depends(require_roles(["Admin", "Almacén"]))
     ]
 )
 def enable_subcategory(
