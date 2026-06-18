1cbf09f4b4f019ca633d45ef850011357035ed18
DevJuan001 <juanesyt7@gmail.com>
2026-06-18 16:32:15 -0500

    fix(database): add unique index on product_serial in OUTPUT_DETAILS table and clean up comments in PRODUCTS insert statements

--- database/01_database.sql
+++ database/01_database.sql
@@ -272,6 +272,7 @@ CREATE TABLE OUTPUT_DETAILS (
   output_details_id INT NOT NULL AUTO_INCREMENT COMMENT ' Identificador de detalle de salida\n,  Clave primaria única que identifica cada registro individual en la tabla de detalles de salida. Este identificador se genera automáticamente por el sistema en orden secuencial y no puede ser nulo. Se utiliza para distinguir y rastrear cada línea o componente dentro de una orden de salida.',
   out_product_garanty DATE NOT NULL COMMENT 'Garantía del producto en salida\n\nIndica si el producto entregado o despachado cuenta con garantía activa o especifica el período de garantía ofrecido. Este campo es obligatorio, ya que permite hacer seguimiento a las condiciones de postventa o soporte del producto.',
   INDEX fk_output_details_output_order_idx (out_order_id ASC),
+  UNIQUE INDEX fk_output_details_product_serial_UNIQUE (product_serial ASC),
   PRIMARY KEY (output_details_id),
   CONSTRAINT fk_output_details_output_oder
     FOREIGN KEY (out_order_id)
